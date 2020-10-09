c9042a93fdc3ba1142e024d8fdb8533af3726b49
Yoshiki Tsutsui <pudgehunter2@hotmail.com>
2020-10-09 17:33:45 -0500

    Porfin, pude solucionar el bug raro de mi pc, y pues ahi empece a terminar las otras soluciones

--- app/src/main/java/co/domi/parcial1/playGame.java
+++ app/src/main/java/co/domi/parcial1/playGame.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import co.domi.parcial1.model.Jugador;
+
 public class playGame extends AppCompatActivity {
 
     Button upButton,leftButton,rightButton,downButton,changeColorButton;
     TCPclient tcp;
+    int x,y;
+    String user,detalles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,51 @@ public class playGame extends AppCompatActivity {
 
        //Singleton
         tcp = TCPclient.getInstance();
+        String a = getIntent().getExtras().getString("a");
+        int b = getIntent().getExtras().getInt("b");
+        int c = getIntent().getExtras().getInt("c");
+        String d = getIntent().getExtras().getString("d");
+        user = a;
+        x = b;
+        y = c;
+        detalles = d;
+
+        downButton.setOnClickListener(
+                (v) -> {
+                    Gson gson = new Gson();
+                    y = y + 50;
+                    Jugador obj = new Jugador(user,x,y,detalles);
+                    String json = gson.toJson(obj);
+                    tcp.Message(json);
+                }
+        );
+        rightButton.setOnClickListener(
+                (v) -> {
+                    Gson gson = new Gson();
+                    x = x + 50;
+                    Jugador obj = new Jugador(user,x,y,detalles);
+                    String json = gson.toJson(obj);
+                    tcp.Message(json);
+                }
+        );
+        leftButton.setOnClickListener(
+                (v) -> {
+                    Gson gson = new Gson();
+                    x = x - 50;
+                    Jugador obj = new Jugador(user,x,y,detalles);
+                    String json = gson.toJson(obj);
+                    tcp.Message(json);
+                }
+        );
+        upButton.setOnClickListener(
+                (v) -> {
+                    Gson gson = new Gson();
+                    y = y - 50;
+                    Jugador obj = new Jugador(user,x,y,detalles);
+                    String json = gson.toJson(obj);
+                    tcp.Message(json);
+                }
+        );
+
     }
 }
